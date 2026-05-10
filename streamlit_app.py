0e03cafd8ebaa55ef37d8a4857364d84f61eea45
Ayyappan Rohith <rsarohith@gmail.com>
2026-05-10 02:51:54 +0000

    Made the basic loop work and sample data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import json
+import random
+
+@st.cache_data(ttl=60)
+def sample_data():
+    return json.load(open("Sample_data.json","r"))
 
 def homepage():
     st.title(":blue[Spellex]! 🐝")
@@ -8,8 +14,8 @@ def homepage():
     "What is your preffered difficulty?",
     ("Super Easy", "Easy", "Medium","Hard","Very Hard","Extreme"),
     index = None,
-    placeholder = "Select one..."
-    )
+    placeholder = "Select one...")
+    
     st.session_state.diffculty = diffculty
 
     deny = True
@@ -17,13 +23,57 @@ def homepage():
 
     if st.button("Start!",icon="📝",icon_position="right",disabled = deny ,type="primary"):
         st.session_state.screen = "question"
+        st.session_state.word_obj = None
         st.rerun()
 
 def question():
-    st.title("<sample>")
-    st.text("<phonetics>, <sample sentence>")
-    st.audio(None)
-    st.text_input("Spelling?")
+    data = sample_data()
+
+    if st.session_state.word_obj == None:
+        while True:
+            word_obj = random.choice(data)
+            
+            if word_obj['difficulty'] == st.session_state.diffculty:
+                break
+        
+        st.session_state.word_obj = word_obj
+
+    word_obj = st.session_state.word_obj
+
+    st.title(f"Spell the word: {word_obj['word']}")
+    st.text(word_obj['sentence'])
+    st.audio(None) #word_obj['audio']
+    
+    ans, button = st.columns([3, 1])
+    st.session_state.answer = ans.text_input("Spelling?").lower()
+
+    button.space()
+    if button.button("Submit",type = "primary"):
+        st.session_state.screen = 'ans_check'
+        st.rerun()
+
+def ans_check():
+    if st.session_state.answer == st.session_state.word_obj['word']:
+        st.title(":green[You got it correct!!!]")
+        st.text(f"You spelled {st.session_state.word_obj['word']} correctly!!")
+        st.balloons()
+
+    else:
+        st.title("Oh no, its wrong...")
+        st.text(f"This is the correct spelling: {st.session_state.word_obj['word']}")
+
+    col1, col2 = st.columns(2)
+
+    if col1.button("Homepage"):
+        st.session_state.screen = 'homepage'
+        st.session_state.word_obj = None
+        st.rerun()
+
+    if col2.button("Continue",type = "primary"):
+        st.session_state.screen = 'question'
+        st.session_state.word_obj = None
+        st.rerun()
+ 
 
 def error_page():
     st.title("Error? 😵‍💫")
@@ -37,6 +87,8 @@ def main():
         st.session_state["session"] = 'created'
         st.session_state['screen'] = 'homepage'
         st.session_state['diffculty'] = None
+        st.session_state['word_obj'] = None
+        st.session_state['answer'] = None
 
     screen = st.session_state.screen
 
@@ -44,6 +96,8 @@ def main():
         homepage()
     elif screen == 'question':
         question()
+    elif screen == 'ans_check':
+        ans_check()
     else:
         error_page()
 
